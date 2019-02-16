69e108eb67bd34fa9f87b23b60e9a75dead97472
Matt <rodriguem3@students.rowan.edu>
2019-02-16 12:11:57 -0500

    New code

--- BikeAI/testAnaconda.py
+++ BikeAI/testAnaconda.py
@@ -1,8 +1,11 @@
 from pandas import *
 from datetime import datetime
+from progressbar import ProgressBar
+
+pbar = ProgressBar()
 
 df = read_csv('bikeShareData.csv')
-dic = { 'StartDate' : [], 'EndDate' : []}
+dic = { 'StartDate': [], 'EndDate': [], 'StartStation': []}
 
 
 def converter(x: datetime):
@@ -15,17 +18,22 @@ for i, row in enumerate(df.values):
     duration, startDate, endDate, startStationNumber, startStationLoc, endSationNumber, endStationLoc, bikeNumber, memberType = row
     dic.get('StartDate').append(startDate[:-3])
     dic.get('EndDate').append(endDate)
+    dic.get('StartStation').append(startStationNumber)
+
 
 newdf = DataFrame(data=dic)
-grouped = newdf.groupby('StartDate')
-parsedDic = { 'DayOfWeek': [], 'Time': [], 'Month': [], 'Demand': []}
-for name, group in grouped:
-    datetimeValue = datetime.strptime(name, '%Y-%m-%d %H:%M')
+grouped = newdf.groupby(['StartDate', 'StartStation'])
+parsedDic = {'StartStation': [], 'DayOfWeek': [], 'Time': [], 'Month': [], 'Demand': []}
+
+for name, group in pbar(grouped):
+    datetimeValue = datetime.strptime(name[0], '%Y-%m-%d %H:%M')
+    parsedDic.get('StartStation').append(name[1])
     parsedDic.get('DayOfWeek').append(datetimeValue.weekday())
     parsedDic.get('Time').append(converter(datetimeValue))
     parsedDic.get('Month').append(datetimeValue.month)
     parsedDic.get('Demand').append(len(group))
     newdf = DataFrame(data=parsedDic)
+
 newdf.to_csv('out.csv')
 
 
