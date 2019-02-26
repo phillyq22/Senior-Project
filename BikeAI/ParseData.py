77a36f7f4825df983a1654190dfb9eb1639ec9fe
Matt <rodriguem3@students.rowan.edu>
2019-02-26 10:11:46 -0500

    Data stuff

--- BikeAI/ParseData.py
+++ BikeAI/ParseData.py
@@ -1,10 +1,11 @@
 from pandas import *
-from datetime import datetime
+from dateutil import rrule
+from datetime import datetime, timedelta
 from progressbar import ProgressBar
 
 pbar = ProgressBar()
 
-df = read_csv('2012Q4-capitalbikeshare-tripdata.csv')
+df = read_csv('unparsedData/2010-capitalbikeshare-tripdata.csv')
 dic = { 'StartDate': [], 'EndDate': [], 'StartStation': []}
 print('Size of input', len(df))
 
@@ -17,9 +18,22 @@ def converter(x: datetime):
 for i, row in enumerate(df.values):
     date = df.index[i]
     duration, startDate, endDate, startStationNumber, startStationLoc, endSationNumber, endStationLoc, bikeNumber, memberType = row
-    dic.get('StartDate').append(startDate[:-3])
+    dic.get('StartDate').append(startDate[:-6])
     dic.get('EndDate').append(endDate)
     dic.get('StartStation').append(startStationNumber)
+
+stationSet = set(dic.get('StartStation'))
+
+print(len(stationSet))
+
+startDay = datetime(2010, 8, 1)
+endYear = datetime(2011, 1, 31)
+
+for stationNum in pbar(stationSet):
+    for dt in rrule.rrule(rrule.HOURLY, dtstart=startDay, until=endYear):
+        dic.get('StartDate').append(str(dt)[:-6])
+        dic.get('EndDate').append(str(dt))
+        dic.get('StartStation').append(stationNum)
 
 
 newdf = DataFrame(data=dic)
@@ -27,17 +41,21 @@ grouped = newdf.groupby(['StartDate', 'StartStation'])
 parsedDic = {'StartStation': [], 'DayOfWeek': [], 'Time': [], 'Month': [], 'Demand': []}
 print('Size after parse', len(grouped))
 
-for name, group in pbar(grouped):
-    datetimeValue = datetime.strptime(name[0], '%Y-%m-%d %H:%M')
+for name, group in grouped:
+    datetimeValue = datetime.strptime(name[0], '%Y-%m-%d %H')
     parsedDic.get('StartStation').append(name[1])
     parsedDic.get('DayOfWeek').append(datetimeValue.weekday())
     parsedDic.get('Time').append(converter(datetimeValue))
     parsedDic.get('Month').append(datetimeValue.month)
-    parsedDic.get('Demand').append(len(group))
+    parsedDic.get('Demand').append(len(group) - 1)
+
+
+
+
 
 newdf = DataFrame(data=parsedDic)
 
-newdf.to_csv('out.csv')
+newdf.to_csv('outSmall.csv')
 
 
 
