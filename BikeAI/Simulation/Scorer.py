e4c40535fed51725fcef333853bcee1ba3e1b940
Zac Capell <capellz0@students.rowan.edu>
2019-04-22 19:16:53 -0400

    Added graphical representation of histogram
    Signed-off-by: Zac Capell <capellz0@students.rowan.edu>

--- BikeAI/Simulation/Scorer.py
+++ BikeAI/Simulation/Scorer.py
@@ -4,7 +4,7 @@
 
 import json
 import statistics
-from collections import Counter
+import matplotlib.pyplot as plt
 
 
 def scorer(path):
@@ -13,19 +13,13 @@ def scorer(path):
     with open(path) as json_file:
         data = json.load(json_file)
         for p in data:
-            ratio.append(p["bikeAvail"] / p["docAvail"])
+            ratio.append(round((p["bikeAvail"] / (p["docAvail"] + p["bikeAvail"])), 2) * 100)
             avail.append(p["bikeAvail"])
-        print("Average bike proportion: " + str(round(statistics.mean(ratio) * 100, 2)) + "%")
+        print("Average bike proportion: " + str(statistics.mean(ratio)) + "%")
         print("Maximum bikes available: " + str(max(avail)))
         print("Minimum bikes available: " + str(min(avail)))
-        histogram(ratio)
-
-
-def histogram(inp):
-    count = Counter(inp)
-    for i in sorted(count):
-        print('{0:5f} {1}'.format(round(i * 100, 2), '+' * count[i]))
-
-
-scorer("data.txt")
-
+        plt.hist(ratio, facecolor='grey', alpha=0.5)
+        plt.xlabel("% of Bikes Available")
+        plt.ylabel("Number of Stations")
+        plt.title("Bike Distribution by Ratio")
+        plt.show()
