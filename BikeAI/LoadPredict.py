c4f4018f576de7e972cb233c938ded4e07316ccf
Andrew Blanco <andrewblanco24@gmail.com>
2019-04-22 17:26:59 -0400

    Add files via upload

--- BikeAI/LoadPredict.py
+++ BikeAI/LoadPredict.py
@@ -2,7 +2,11 @@ from pandas import *
 import pickle
 
 reg = pickle.load( open( "file", "rb"))
-dic = {'Unnamed:' : [1],  'DayOfWeek' : [2], 'Month' : [3],  'StartStation' : [31909],  'Time': [300]}
-newdf = DataFrame(data=dic)
 
-print(reg.predict(newdf).round())
+
+def testSingleStation(dayOfWeek, month, startStation, time, maxSusWind, maxTemp, maxWind, meandDew, meanTemp, meanWind, minTemp, snowDepth, totalPrec, visibility) :
+    dic = {'DayOfWeek': [dayOfWeek], 'Month': [month], 'StartStation': [startStation], 'Time': [time],
+           'Max sus Wind': [maxSusWind], 'Max Temp': [maxTemp], 'MaxWind': [maxWind], 'meanDew': [meandDew], 'meanTemp': [meanTemp],
+           'meanWind': [meanWind], 'minTemp': [minTemp], 'snowDepth': [snowDepth], 'totalPrec': [totalPrec], 'visibility': [visibility]}
+    newdf = DataFrame(data=dic)
+    return reg.predict(newdf).round()
