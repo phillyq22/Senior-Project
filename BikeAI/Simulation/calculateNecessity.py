81f62b6211ee0ca2626522263d3d7d379fb8cc56
Philip Quinn <quinnp6@students.rowan.edu>
2019-05-06 13:18:03 -0400

    Updated

--- BikeAI/Simulation/calculateNecessity.py
+++
@@ -1,8 +0,0 @@
-#capacity, numBikesAvailable, demand, threshold
-
-def calculate_necessity(capacity, numBikesAvailable, demand, threshold):
-    target = capacity * threshold
-    return target - (numBikesAvailable - demand)
-
-
-print(calculate_necessity(25, 12, 2, 0.8))
