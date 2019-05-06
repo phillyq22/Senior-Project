427d9cccc911adb28347da2b8aae98197e2b19a7
Philip Quinn <quinnp6@students.rowan.edu>
2019-05-06 11:32:11 -0400

    Updated documentation

--- BikeAI/Simulation/User.py
+++ BikeAI/Simulation/User.py
@@ -1,6 +1,8 @@
 import json
 
-
+'''
+User object to represent CapitalBikeShare users.
+'''
 class User:
     def __init__(self, startStation, endStation, endLocation, endDateTime, startLocation, jsonText=-1):
 
@@ -8,11 +10,11 @@ class User:
             self.__dict__ = json.loads(jsonText)
 
         else:
-            self.startStation = startStation
-            self.endStation = endStation
-            self.endLocation = endLocation
-            self.endDateTime = endDateTime
-            self.startLocation = startLocation
+            self.startStation = startStation # station bike is picked up from
+            self.endStation = endStation # station bike is dropped off
+            self.endLocation = endLocation # Where the user departs from to go to end station
+            self.endDateTime = endDateTime # Time that the bike is dropped off
+            self.startLocation = startLocation # Where the user departs from to go to the start station
 
     def __str__(self):
         return 'Start Station: ' + str(self.startStation) + ' End Station: ' + str(self.endStation) + ' End Location: ' \
